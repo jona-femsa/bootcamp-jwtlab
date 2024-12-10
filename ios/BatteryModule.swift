c8c79a769b2f779c29ba371a953952894de47380
Jonathan Salinas <jonathan.salinas@digitalfemsa.com>
2024-12-10 13:42:06 -0600

    lab: ios native

---
+++ ios/BatteryModule.swift
@@ -0,0 +1,30 @@
+//
+//  BatteryModule.swift
+//  JwtApp
+//
+//  Created by Jonathan Salinas on 10/12/24.
+//
+
+import Foundation
+import React
+
+@objc(BatteryModule)
+class BatteryModule: NSObject {
+    @objc
+    static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+
+    @objc
+    func getBatteryLevel(_ resolve: @escaping RCTPromiseResolveBlock,
+                         reject: @escaping RCTPromiseRejectBlock) {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let batteryLevel = UIDevice.current.batteryLevel
+        if batteryLevel >= 0 {
+            resolve(Int(batteryLevel * 100))
+        } else {
+            let error = NSError(domain: "", code: 200, userInfo: nil)
+            reject("E_BATTERY_LEVEL", "Battery level unavailable", error)
+        }
+    }
+}
